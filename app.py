d7accaa895fd8fc42f7f9532415cd7370c6a4752
Evgenii Zorin <zorin.evgenii.m@gmail.com>
2023-03-25 17:10:11 -0600

    v 1.6

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, url_for, redirect, send_file
 app = Flask(__name__)
 # Markdown(app)
 
-@app.route('/') # Decorator
+@app.route('/')
 @app.route('/home')
 def home_route():
 	return render_template('home.html')
@@ -15,12 +15,13 @@ def portfolio():
 
 @app.route('/blog')
 def blog():
-	return render_template('blog.html')
+	list_example = ['entry1', 'entry2', 'entry3']
+	return render_template('blog.html', list_example=list_example)
 
 @app.route('/blog/blog_entry/<string:post>')
 def blog_entry(post):
 	print(f"Argument passed: {post}")
-	return render_template(f'blog_entry_{post}.html')
+	return render_template(f'blog_entry_{post}.html', post_number=post)
 
 @app.route('/about')
 def about():
